24e16bc60dc163ced68599d7d547be46d357c389
Tsukuyomi-808 <froilanxx3@gmail.com>
2022-08-01 09:35:55 -1000

    commit

--- CoreAssignment/collectionpractice/Program.cs
+++ CoreAssignment/collectionpractice/Program.cs
@@ -1,4 +1,32 @@
-﻿
+﻿// Collections Practice Assignment
+
+// 1. Three basic Arrays 
+// 1.1 Create an array to hold interger values 0 through 9
+// 1.2 Create an array the names "Tim","Martin","Nikki",& "Sara"
+// 1.3 Create an array of length 10 that alternates between true and false values, starting with true
+
+int[] array1 = {0,1,2,3,4,5,6,7,8,9};
+string[] array2 = {"Tim", "Martin", "Nick", "Sara"};
+bool[] array3 = new bool[10];
+array3[0] = true;
+array3[1] = false;
+array3[2] = true;
+array3[3] = false;
+array3[4] = true;
+array3[5] = false;
+array3[6] = false;
+array3[7] = true;
+array3[8] = false;
+array3[9] = true;
+
+
+
+
+
+
+
+
+
 
 List<string> flavors = new List<string>() {
   "cherry garcia",
@@ -10,7 +38,7 @@ List<string> flavors = new List<string>() {
 
 flavors.Add("caramel");
 flavors.RemoveAt(4);
-flavors.Insert(2,"caramel");
+flavors.Insert(2, "caramel");
 
 Console.WriteLine(flavors.Count);
 Console.WriteLine(flavors[1]);
@@ -23,16 +51,16 @@ foreach (string a in flavors)
 // a is a variable it can be named anything
 
 List<int> orderedNums = new List<int>() {
-  1,2,3,4,5,6,7,8,9,10,11,12,13 
+  1,2,3,4,5,6,7,8,9,10,11,12,13
 };
 
-List<int> shuffleNums = new List<int>(); 
+List<int> shuffleNums = new List<int>();
 
-Random rand= new Random();
+Random rand = new Random();
 
 // Console.WriteLine(rand.Next(0,15));
 
-while (orderedNums.Count > 0) 
+while (orderedNums.Count > 0)
 {
   int randIdx = rand.Next(0, orderedNums.Count);
   shuffleNums.Add(orderedNums[randIdx]);
@@ -43,3 +71,14 @@ foreach (int num in shuffleNums)
 {
   Console.WriteLine(num);
 }
+
+
+Dictionary<string, string> someDictionary = new Dictionary<string, string>() {
+  {"name", "joseph"},
+  {"favorite_color", "purple"},
+  {"23", "jonas"}
+};
+
+Console.WriteLine(someDictionary["name"]);
+Console.WriteLine(someDictionary["23"]);
+Console.WriteLine(someDictionary["favorite_color"]);
