23927dcaa38e322b0f347a860b50c4d8a25a9b21
Tsukuyomi-808 <froilanxx3@gmail.com>
2022-07-31 20:42:39 -1000

    Commit Practice assignment

---
+++ Algos/practice.js
@@ -0,0 +1,5 @@
+function addition (a,b) {
+  const sum = 1000 + 10;
+  return sum // didnt know i had to put a return here
+}
+console.log(addition());// and i thought we console log addition(a,b) but its working with just an empty ()
