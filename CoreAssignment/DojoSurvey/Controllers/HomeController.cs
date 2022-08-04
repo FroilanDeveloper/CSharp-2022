1d01bd9cc265ebc0bb28dac609dc40fbd0607d09
Tsukuyomi-808 <froilanxx3@gmail.com>
2022-08-04 12:42:55 -1000

    commit

---
+++ CoreAssignment/DojoSurvey/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+using Survey.Models;
+
+public class HomeController : Controller
+{
+  [HttpGet("/")]
+  public IActionResult Survey()
+  {
+    return View();
+  }
+
+  [HttpPost("/survey")]
+  public IActionResult Result(HomeSurvey  newSubmitted) // Dotnet takes the field from our form and creates an object base on the view model that we defined
+  {
+    return View(newSubmitted);
+  }
+}
+
---
+++ CoreAssignment/DojoSurvey/Models/HomeModel.cs
@@ -0,0 +1,12 @@
+#pragma warning disable CS8618
+
+namespace Survey.Models;
+
+public class HomeSurvey
+{
+  public string FullName { get; set; }
+  public string Location { get; set; }
+  public string Language { get; set; }
+  public string Comment { get; set; }
+}
+
