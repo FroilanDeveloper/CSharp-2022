b6653cec9174a7fa721874a0cbfa7661a01c9043
Tsukuyomi-808 <froilanxx3@gmail.com>
2022-08-08 07:26:30 -1000

    commit

--- CoreAssignment/DojoSurvey/Models/HomeModel.cs
+++ CoreAssignment/DojoSurvey/Models/HomeModel.cs
@@ -6,20 +6,17 @@ namespace Survey.Models;
 
 public class HomeSurvey
 {
-  [Required(ErrorMessage = "is Required")]
-  [MinLength(2, ErrorMessage = "Atleast 2 characters long")]
-  [Display(Name = "Username")]
-  public string FullName { get; set; }
   [Required]
+  [MinLength(2, ErrorMessage = "Atleast 2 characters long")]
+  public string FullName { get; set; }
   [Display(Name ="Dojo Location")]
   public string Location { get; set; }
-  [Required]
+
   [Display(Name ="Favorite Language")]
 
   public string Language { get; set; }
-  [Required]
-  [Display(Name ="Comment")]
+
   [MinLength(20, ErrorMessage ="must be at least 20 characters long")]
-  public string Comment { get; set; }
+  public string? Comment { get; set; }
 }
 
