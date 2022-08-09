da4f1559f0cfdcc4f18c5ad6af6c6f34be716484
Tsukuyomi-808 <froilanxx3@gmail.com>
2022-08-09 09:41:58 -1000

    commit

--- CoreAssignment/DojoSurveyValidation/Models/Survey.cs
+++ CoreAssignment/DojoSurveyValidation/Models/Survey.cs
@@ -10,8 +10,10 @@ public class Survey
   [MinLength(2, ErrorMessage = "Atleast 2 characters long!")]
   public string FullName { get; set; }
 
+  [Required (ErrorMessage = "Location is required!")]
   public string Location { get; set; }
 
+  [Required (ErrorMessage = "Language is required!")]
   public string Language { get; set; }
 
   [MinLength(20, ErrorMessage = "Must be atleast 20 characters long")]
