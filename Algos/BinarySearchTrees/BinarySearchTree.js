317ab645ad8a9f553d8150b2a9c64e25f0e445f2
Tsukuyomi-808 <froilanxx3@gmail.com>
2022-08-10 22:23:23 -1000

    commit

--- Algos/BinarySearchTrees/BinarySearchTree.js
+++ Algos/BinarySearchTrees/BinarySearchTree.js
@@ -1,7 +1,7 @@
 /**
  * Class to represent a Node in a Binary Search Tree (BST).
  */
-  class BSTNode {
+class BSTNode {
   /**
    * Constructs a new instance of a BST node.
    * @param {number} data The integer to store in the node.
@@ -24,9 +24,9 @@
 }
 
 /**
-* Represents an ordered tree of nodes where the data of left nodes are <= to
-* their parent and the data of nodes to the right are > their parent's data.
-*/
+ * Represents an ordered tree of nodes where the data of left nodes are <= to
+ * their parent and the data of nodes to the right are > their parent's data.
+ */
 class BinarySearchTree {
   constructor() {
     /**
@@ -39,9 +39,112 @@ class BinarySearchTree {
   }
 
   /**
-   * Determines if this tree is empty.
+   * Inserts a new node with the given newVal in the right place to preserver
+   * the order of this tree.
    * - Time: O(?).
    * - Space: O(?).
+   * @param {number} newVal The data to be added to a new node.
+   * @returns {BinarySearchTree} This tree.
+   */
+  insert(newVal) {
+    const newNode = new BSTNode(newVal);
+    if(this.isEmpty){
+      this.root = newNode;
+      return this;
+    }
+    let runner = this.root;
+    while(runner != newNode){
+      if(runner.data < newNode.data){
+        if (runner.right == null) {
+          runner.right = newNode;
+        } else {
+          runner = runner.right
+        } else if 
+      }
+    }
+  }
+
+  /**
+   * Inserts a new node with the given newVal in the right place to preserver
+   * the order of this tree.
+   * - Time: O(?).
+   * - Space: O(?).
+   * @param {number} newVal The data to be added to a new node.
+   * @param {Node} curr The node that is currently accessed from the tree as
+   *    the tree is being traversed.
+   * @returns {BinarySearchTree} This tree.
+   */
+  insertRecursive(newVal, curr = this.root) {}
+
+  /**
+   * Determines if this tree contains the given searchVal.
+   * - Time: O(h) linear, h = height of tree.
+   * - Space: O(1) constant.
+   * @param {number} searchVal The number to search for in the node's data.
+   * @returns {boolean} Indicates if the searchVal was found.
+   */
+  contains(searchVal) {
+    let current = this.root;
+
+    while (current) {
+      if (current.data === searchVal) {
+        return true;
+      }
+
+      if (searchVal < current.data) {
+        current = current.left;
+      } else {
+        current = current.right;
+      }
+    }
+    return false;
+  }
+
+  /**
+   * Determines if this tree contains the given searchVal.
+   * - Time: O(h) linear, h = height of tree.
+   * - Space: O(h) linear due to the call stack.
+   * @param {number} searchVal The number to search for in the node's data.
+   * @returns {boolean} Indicates if the searchVal was found.
+   */
+  containsRecursive(searchVal, current = this.root) {
+    if (current === null) {
+      return false;
+    }
+
+    if (current.data === searchVal) {
+      return true;
+    }
+
+    if (searchVal < current.data) {
+      return this.containsRecursive(searchVal, current.left);
+    }
+
+    if (searchVal > current.data) {
+      return this.containsRecursive(searchVal, current.right);
+    }
+  }
+
+  /**
+   * Calculates the range (max - min) from the given startNode.
+   * - Time: O(rightHeight + leftHeight) -> still linear so simplify to O(h).
+   * - Space: O(h) linear due to the call stack. The max side finishes before
+   *    the right min side is added to the stack.
+   * @param {BSTNode} startNode The node to start from to calculate the range.
+   * @returns {number|null} The range of this tree or a sub tree depending on if the
+   *    startNode is the root or not.
+   */
+  range(startNode = this.root) {
+    if (!startNode) {
+      return null;
+    }
+    return this.max(startNode) - this.min(startNode);
+  }
+
+  /**
+   * Determines if this tree is empty.
+   * - Time: O(1) constant.
+   * - Space: O(1) constant.
    * @returns {boolean} Indicates if this tree is empty.
    */
   isEmpty() {
@@ -50,48 +153,79 @@ class BinarySearchTree {
 
   /**
    * Retrieves the smallest integer data from this tree.
-   * - Time: O(?).
-   * - Space: O(?).
-   * @param {Node} current The node that is currently accessed from the tree as
+   * - Time: O(h) linear, h = height of left sub tree starting from current node.
+   * - Space: O(1) constant.
+   * @param {BSTNode} current The node that is currently accessed from the tree as
    *    the tree is being traversed.
    * @returns {number} The smallest integer from this tree.
    */
   min(current = this.root) {
-    while (! current.left.data === null){
+    if (current === null) {
+      return null;
+    }
+
+    while (current.left) {
       current = current.left;
-    } 
+    }
     return current.data;
   }
 
   /**
    * Retrieves the smallest integer data from this tree.
-   * - Time: O(?).
-   * - Space: O(?).
-   * @param {Node} current The node that is currently accessed from the tree as
+   * - Time: O(h) linear, h = height of left sub tree starting from current node.
+   * - Space: O(h) linear due to the call stack.
+   * @param {BSTNode} current The node that is currently accessed from the tree as
    *    the tree is being traversed.
    * @returns {number} The smallest integer from this tree.
    */
-  minRecursive(current = this.root) {}
+  minRecursive(current = this.root) {
+    if (current === null) {
+      return null;
+    }
+
+    if (current.left === null) {
+      return current.data;
+    }
+    return this.minRecursive(current.left);
+  }
 
   /**
    * Retrieves the largest integer data from this tree.
-   * - Time: O(?).
-   * - Space: O(?).
-   * @param {Node} current The node that is currently accessed from the tree as
+   * - Time: O(h) linear, h = height of right sub tree starting from current node.
+   * - Space: O(1) constant.
+   * @param {BSTNode} current The node that is currently accessed from the tree as
    *    the tree is being traversed.
    * @returns {number} The largest integer from this tree.
    */
-  max(current = this.root) {}
+  max(current = this.root) {
+    if (current === null) {
+      return null;
+    }
+
+    while (current.right) {
+      current = current.right;
+    }
+    return current.data;
+  }
 
   /**
    * Retrieves the largest integer data from this tree.
-   * - Time: O(?).
-   * - Space: O(?).
-   * @param {Node} current The node that is currently accessed from the tree as
+   * - Time: O(h) linear, h = height of right sub tree starting from current node.
+   * - Space: O(h) linear due to the call stack.
+   * @param {BSTNode} current The node that is currently accessed from the tree as
    *    the tree is being traversed.
    * @returns {number} The largest integer from this tree.
    */
-  maxRecursive(current = this.root) {}
+  maxRecursive(current = this.root) {
+    if (current === null) {
+      return null;
+    }
+
+    if (current.right === null) {
+      return current.data;
+    }
+    return this.maxRecursive(current.right);
+  }
 
   // Logs this tree horizontally with the root on the left.
   print(node = this.root, spaceCnt = 0, spaceIncr = 10) {
@@ -144,13 +278,13 @@ threeLevelTree.root.right.left = new BSTNode(13);
 
 /* fullTree
                   root
-              <-- 25 -->
-          /            \
-          15             50
-      /    \         /    \
-      10     22      35     70
-  /   \   /  \    /  \   /  \
-  4    12  18  24  31  44 66  90
+            <-- 25 -->
+          /             \
+        15             50
+      /    \           /    \
+    10     22       35     70
+  /   \     /  \    /  \   /  \
+  4    12  18  24   31  44 66  90
 */
 /***************** Uncomment after insert method is created. ****************/
 // const fullTree = new BinarySearchTree();
--- CoreAssignment/LINQEruption/Program.cs
+++ CoreAssignment/LINQEruption/Program.cs
@@ -1,24 +1,19 @@
 ﻿List<Eruption> eruptions = new List<Eruption>()
 {
-    new Eruption("La Palma", 2021, "Canary Is", 2426, "Stratovolcano"),
-    new Eruption("Villarrica", 1963, "Chile", 2847, "Stratovolcano"),
-    new Eruption("Chaiten", 2008, "Chile", 1122, "Caldera"),
-    new Eruption("Kilauea", 2018, "Hawaiian Is", 1122, "Shield Volcano"),
-    new Eruption("Hekla", 1206, "Iceland", 1490, "Stratovolcano"),
-    new Eruption("Taupo", 1910, "New Zealand", 760, "Caldera"),
-    new Eruption("Lengai, Ol Doinyo", 1927, "Tanzania", 2962, "Stratovolcano"),
-    new Eruption("Santorini", 46,"Greece", 367, "Shield Volcano"),
-    new Eruption("Katla", 950, "Iceland", 1490, "Subglacial Volcano"),
-    new Eruption("Aira", 766, "Japan", 1117, "Stratovolcano"),
-    new Eruption("Ceboruco", 930, "Mexico", 2280, "Stratovolcano"),
-    new Eruption("Etna", 1329, "Italy", 3320, "Stratovolcano"),
-new Eruption("Bardarbunga", 1477, "Iceland", 2000, "Stratovolcano")
+  new Eruption("La Palma", 2021, "Canary Is", 2426, "Stratovolcano"),
+  new Eruption("Villarrica", 1963, "Chile", 2847, "Stratovolcano"),
+  new Eruption("Chaiten", 2008, "Chile", 1122, "Caldera"),
+  new Eruption("Kilauea", 2018, "Hawaiian Is", 1122, "Shield Volcano"),
+  new Eruption("Hekla", 1206, "Iceland", 1490, "Stratovolcano"),
+  new Eruption("Taupo", 1910, "New Zealand", 760, "Caldera"),
+  new Eruption("Lengai, Ol Doinyo", 1927, "Tanzania", 2962, "Stratovolcano"),
+  new Eruption("Santorini", 46,"Greece", 367, "Shield Volcano"),
+  new Eruption("Katla", 950, "Iceland", 1490, "Subglacial Volcano"),
+  new Eruption("Aira", 766, "Japan", 1117, "Stratovolcano"),
+  new Eruption("Ceboruco", 930, "Mexico", 2280, "Stratovolcano"),
+  new Eruption("Etna", 1329, "Italy", 3320, "Stratovolcano"),
+  new Eruption("Bardarbunga", 1477, "Iceland", 2000, "Stratovolcano")
 };
-// Example Query - Prints all Stratovolcano eruptions
-IEnumerable<Eruption> stratovolcanoEruptions = eruptions.Where(c => c.Type == "Stratovolcano");
-PrintEach(stratovolcanoEruptions, "Stratovolcano eruptions.");
-// Execute Assignment Tasks here!
-
 // Helper method to print each item in a List or IEnumerable.This should remain at the bottom of your class!
 static void PrintEach(IEnumerable<dynamic> items, string msg = "")
 {
@@ -28,3 +23,69 @@ static void PrintEach(IEnumerable<dynamic> items, string msg = "")
     Console.WriteLine(item.ToString());
   }
 }
+// Example Query - Prints all Stratovolcano eruptions
+IEnumerable<Eruption> stratovolcanoEruptions = eruptions.Where(c => c.Type == "Stratovolcano");
+// PrintEach(stratovolcanoEruptions, "Stratovolcano eruptions.");
+// Execute Assignment Tasks here!
+
+
+// 1. Use LINQ to find the first eruptions that is in Chile and print the result.
+Console.WriteLine("1");
+IEnumerable<Eruption> firstChileEruptions = eruptions.Where(c => c.Location == "Chile");
+// PrintEach(firstChileEruptions, "Chile");
+
+Console.WriteLine("*******************************");
+
+// 2. Find the first eruptions from the "Hawaiian Is" location and print it. If none is found, print "No Hawaiian Is Eruption found."
+Console.WriteLine("2");
+IEnumerable<Eruption>  firstHawaiianEruptions = eruptions.Where(c => c.Location == "Hawaiian Is");
+if (firstHawaiianEruptions == null)
+{
+  Console.WriteLine("No Hawaiian Is Eruption found.");
+}
+PrintEach(firstHawaiianEruptions, "Hawaiian Is");
+
+Console.WriteLine("*******************************");
+
+// 3. Find the first eruptions that is after the year 1900 AND "New Zealand", then print it.
+IEnumerable<Eruption> firstNewZealandEruptions = eruptions.Where(c => c.Location == "New Zealand");
+PrintEach(firstNewZealandEruptions, "New Zealand");
+
+Console.WriteLine("*******************************");
+
+// 4. Find all eruptions where the volcano's elevation is over 2000m and print them.
+IEnumerable<Eruption> allEruptionsOver2000m = eruptions.Where(c => c.ElevationInMeters >= 2000);
+PrintEach(allEruptionsOver2000m, "2000m");
+
+Console.WriteLine("*******************************");
+
+// 5. Find all eruptions where the volcano's name starts with "Z" and print them. Also print the number of eruptions found.
+IEnumerable<Eruption> allZNameEruptions = eruptions.Where(c => c.Volcano.StartsWith("Z"));
+int count = allZNameEruptions.Count();
+PrintEach(allZNameEruptions, $"{count} was found");
+
+Console.WriteLine("*******************************");
+
+// 6. Find the highest elevation, and print only that integer.
+int highestElevation = eruptions.Max(c => c.ElevationInMeters);
+Console.WriteLine(highestElevation);
+
+Console.WriteLine("*******************************");
+
+// 7. Use the highest elevation variable to find a print the name of the Volcano with that elevation.
+Eruption highestElevationName = eruptions.Where(c => c.ElevationInMeters == highestElevation).First();
+Console.WriteLine(highestElevationName.Volcano);
+
+Console.WriteLine("*******************************");
+
+// 8. Print all Volcano names alphabetically.
+IEnumerable<Eruption> alphabetically = eruptions.OrderBy(c => c.Volcano).ToList();
+PrintEach(alphabetically);
+
+
+Console.WriteLine("*******************************");
+
+// 9. Print all the eruptions that happened before the year 1000 CE alphabetically according to Volcano name.
+IEnumerable<Eruption> before1000Alphabetically = eruptions.Where(c => c.Year <= 1000).OrderBy(c => c.Volcano).ToList();
+PrintEach(before1000Alphabetically);
+
